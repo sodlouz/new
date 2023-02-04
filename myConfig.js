09466c1586be0e4af369b1af7c8b2e53a3a3dd3d
sodlouz <110484372+sodlouz@users.noreply.github.com>
2023-02-04 16:13:44 +0100

    Update myConfig.js

--- myConfig.js
+++ myConfig.js
@@ -1,56 +1,28 @@
 let MensajeAd =
-	"​🚨*Promociona lo que desees!*🚨 \n \n" +
-	"	🎁	- Web de afiliados / referidos \n" +
-	"	🤑	- Acortador con AdsTerra \n" +
-	"	📈	- Página con exoclick \n" +
-	"	🗺	- Redirige el tráfico a @otrosCanales o a una página web \n \n" +
-	"['Click Aquí🔥'](https://www.amazon.es)"
+	"​🚨*Im LIVE*🚨 \n \n" +
+	"	🎁	- sherrysex.fun" +
 
 /*let MensajeAd3 =
 	"​🚨*Promociona lo que desees!*🚨 \n \n"*/
 
-let imagenAd =
-	"https://vilmanunez.com/wp-content/uploads/2018/10/poner-anuncios-publicitarios-gratis.png"
 
-const N_VIDEOS = 10 //MAX 10
+const N_VIDEOS = 5 //MAX 10
 
 const MY_AD = {
-	isActive: false,
-	type: 3, // //defaultAd => 1 //captionAd => 2 //linksGroupAd => 3
+	isActive: true,
+	type: 2, // //defaultAd => 1 //captionAd => 2 //linksGroupAd => 3
 	data: {
 		ctn: MensajeAd,
-		image: imagenAd,
 		link: {
-			text: "Canal vip ❤",
-			url: "https://google.com",
+			text: "🚨*Im LIVE*🚨",
+			url: "sherrysex.fun",
 		},
 		linksGroupAd: [
 			[
 				{
-					text: "Web Afiliados / Referidos",
-					url: "https://youtube.com",
+					text: "🚨*Im LIVE*🚨",
+					url: "sherrysex.fun",
 				},
-			],
-			[
-				{
-					text: "Acortador de enlaces",
-					url: "https://facebook.com",
-				},
-			],
-			[
-				{
-					text: "Web con anuncios",
-					url: "https://stripchat.com",
-				},
-			],
-			[
-				{
-					text: "Promociona otro canal",
-					url: "https://tiktok.com",
-				},
-			],
-		],
-	},
 }
 
 module.exports = { N_VIDEOS, MY_AD }
