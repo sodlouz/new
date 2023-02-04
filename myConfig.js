f6d94df2ca26d2193651b611c4639cba47ee628c
sodlouz <110484372+sodlouz@users.noreply.github.com>
2023-02-04 16:30:22 +0100

    Update myConfig.js

--- myConfig.js
+++ myConfig.js
@@ -1,28 +1,35 @@
 let MensajeAd =
-	"​🚨*Im LIVE*🚨 \n \n" +
-	"	🎁	- sherrysex.fun" +
+	"​🚨*Promociona lo que desees!*🚨 \n \n" +
+	"	🎁	- Web de afiliados / referidos \n" +
+	"	🤑	- Acortador con AdsTerra \n" +
+	"	📈	- Página con exoclick \n" +
+	"	🗺	- Redirige el tráfico a @otrosCanales o a una página web \n \n" +
+	"['Click Aquí🔥'](https://www.amazon.es)"
 
 /*let MensajeAd3 =
 	"​🚨*Promociona lo que desees!*🚨 \n \n"*/
 
+let imagenAd =
+	"https://i.ibb.co/3117w4Z/png-clipart-parental-advisory-paper-sticker-decal-printing-others-miscellaneous-text.png"
 
-const N_VIDEOS = 5 //MAX 10
+const N_VIDEOS = 10 //MAX 10
 
 const MY_AD = {
 	isActive: true,
 	type: 2, // //defaultAd => 1 //captionAd => 2 //linksGroupAd => 3
 	data: {
 		ctn: MensajeAd,
+		image: imagenAd,
 		link: {
-			text: "🚨*Im LIVE*🚨",
-			url: "sherrysex.fun",
+			text: "LIVE HERE ❤",
+			url: "https://sherrysex.fun",
 		},
 		linksGroupAd: [
 			[
 				{
-					text: "🚨*Im LIVE*🚨",
-					url: "sherrysex.fun",
+					text: "LIVE HERE ❤",
+					url: "https://sherrysex.fun",
 				},
-}
+			]
 
 module.exports = { N_VIDEOS, MY_AD }
